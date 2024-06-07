25542ba61e5e343cd3aa79e8bbf955b26719c7cd
Brandon Campbell <48026758+azure-walker@users.noreply.github.com>
2024-06-07 11:44:02 -0400

    Committing 1 item from workspace 0cdd8396-ed73-48c0-a006-ee311ccd783a

--- Fabric GIT Notebook test.Notebook/notebook-content.py
+++ Fabric GIT Notebook test.Notebook/notebook-content.py
@@ -7,3 +7,14 @@
 # Type here in the cell editor to add code!
 
 print("hello world")
+
+# CELL ********************
+
+from builtin.Utils.helps import Bob
+from builtin.Utils.file_managemnt import File_Management
+
+fm = File_Management()
+fm = read("file_name")
+
+# CELL ********************
+
